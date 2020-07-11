436e43b0b78eeabb810d6d75a0b7de2c9c3242c2
notapineapple <damnyorch@gmail.com>
2020-07-11 18:26:23 -0500

    [MODIFIED] final exercise

--- graphql_basic/server.js
+++ graphql_basic/server.js
@@ -1,12 +1,75 @@
 const { ApolloServer, gql } = require('apollo-server')
-// const data = require('./data/')
+const data = require('./data/1.json')
 
 const typeDefs = gql`
-  # definitions
+  enum Gender {
+    Male
+    Female
+    NA
+    None
+    Hermaphrodite
+  }
+
+  type Character {
+    name: String!
+    height: String!
+    mass: String!
+    gender: Gender!
+    species: String!
+  }
+
+  type CharacterResult {
+    characters: [Character]!
+    totalCount: Int!
+  }
+
+  type Query {
+    character: Character!
+    characters(gender: Gender!): CharacterResult
+  }
 `
 
 const resolvers = {
-  // resolvers
+  Gender: {
+    Male: 'male',
+    Female: 'female',
+    NA: 'NA',
+    None: 'none',
+    Hermaphrodite: 'hermaphrodite'
+  },
+  Query: {
+    character() {
+      return data.pop()
+    },
+    characters(_, { gender }) {
+      return gender ? (data || []).filter(character => character.gender === gender) : data
+    }
+  },
+  CharacterResult: {
+    characters(parent) {
+      return parent
+    },
+    totalCount(parent) {
+      return parent.length
+    }
+  },
+  Character: {
+    name(parent) {
+        return parent.name
+    },
+    height(parent) {
+      return parent.height
+    },
+    mass(parent) {
+      return parent.mass
+    },
+    gender(parent) {
+      return parent.gender
+    },
+    species(parent) {
+      return parent.species
+    }
+  }
 }
 
 
